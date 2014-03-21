e21370100e55f755a0107d69be7795b729b15f35
Niklas Hallberg <8715.hallberg@gmail.com>
2014-03-21 17:16:31 +0100

    update on frontend.h and backend.h

--- src/backend.h
+++ src/backend.h
@@ -1,25 +1,24 @@
-#ifndef BACKEND
-#define BACKEND
+#ifndef BACKEND_H
+#define BACKEND_H
 
 #include "newsgroup.h"
-#indlude <string>
+#include <string>
 #include <vector>
 #include <map>
 
-class BackEnd (
+class BackEnd {
 	public;
-	bool listNG(std::vector<std::pair<int, std::string>>&);
-	bool addNG(std::string name);
-	bool removeNG(int id);
-	bool listArticle(std::vector<std::pair<int, std::string>>&);
-	bool addArticle(std::vector<std::string<);
-	bool removeArticle(int id);
+	bool listNG(std::vector<std::pair<int, std::string>>& ngs);
+	bool addNG(const std::string& name);
+	bool removeNG(const int id);
+	bool listArticle(std::vector<std::pair<int, std::string>>& arts);
+	bool addArticle(const std::string& title, const std::string& author, const std::string& textbody);
+	bool removeArticle(const int id);
 	
 	private:
 	std::map<int, NewsGroup::NewsGroup>;
 	int NGcount;
-	
 
-);
+};
 
 #endif
--- src/frontend.h
+++ src/frontend.h
@@ -1,10 +1,16 @@
 #ifndef FRONTEND_H
 #define FRONTEND_H
 
+#include "connection.h"
+
 class FrontEnd {
 public:
 	Frontend(BackEnd& be) : backend(be) {}
-	void send
+	void readAndReply(const shared_ptr<Connection>& conn);
+private:
+	BackEnd backend;
+
+	//lots of private methods
 };
 
 #endif
