20417dc34a24d5ded5398d0a7300a0be8f40760c
Viktor Nybom <flumbom@gmail.com>
2014-04-06 20:58:19 +0200

    Nu funkar alla test för zerver. dvs backend.cc har fixats med dubleter av newsgroups

--- src/backend.cc
+++ src/backend.cc
@@ -4,6 +4,7 @@
 #include "backend.h"
 #include "article.h"
 #include "newsgroup.h"
+#include <algorithm>
 
 
 using namespace std;
@@ -17,14 +18,11 @@ void BackEnd::listNG(std::vector<std::pair<int, std::string>>& ngs){
 }
 
 bool BackEnd::addNG(const std::string& name){
-	auto it = database.find_if(database.begin(), database.end(),
-		[name] (auto it) { return (it->second).getTitle() == name; });
-	if (it == database.end()) {
-		database.insert(++ng_count, NewsGroup(ng_count, name));
-		return true;
-	} else {
-		return false;
+	for (auto it = database.begin(); it != database.end(); ++it ) {
+		if (it->second.getTitle() == name) return false;
 	}
+	database.emplace(++ng_count, NewsGroup(ng_count, name));
+	return true; 
 }
 
 bool BackEnd::removeNG(const int id){
